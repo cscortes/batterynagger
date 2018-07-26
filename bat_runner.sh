77a6602097b718d8eedb27331838b0887a8ecca8
Luis <mrluiscortes@gmail.com>
2018-07-26 16:35:55 -0600

    Changed directory.  Need to find a better way to do this.

--- bat_runner.sh
+++ bat_runner.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
-cd /home/cscortes/Documents/github/batterynagger
+#
+# sample startup script for your login
+# on Fedora, look for startup applications 
+# in your start menu. 
+# then point your task to this routine
+# All this does, is makes sure we are running it from 
+# the correct path, before we execute it.  
+#
+# @TODO: There is probably a better way
+#
+
+cd /home/cscortes/Documents/GitHub/batterynagger
 python3 wbatterynagger.py 
